16f2d647dd338d68d95b732b1d17e4500fe44b30
Meredith Leu <meredith.leu@gmail.com>
2013-10-09 19:03:43 -0700

    Add Securesocial master-SNAPSHOT to the app

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,10 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "securesocial" %% "securesocial" % "master-SNAPSHOT"
   )
-
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
   )
 
 }
